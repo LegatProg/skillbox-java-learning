54c5a9c7b049cd474b1f2bc47e30170391447957
LegatProg <yazza@mail.ru>
2019-04-29 18:27:12 +0500

    4_2 done

---
+++ modul_4/4_2/ValuesPrinter/src/ValuesPrinter.java
@@ -0,0 +1,26 @@
+import java.math.BigDecimal;
+
+public class ValuesPrinter {
+    public static void main(String[] args) {
+
+
+        System.out.println("\nDouble min: " + Double.MIN_VALUE);
+        System.out.println("Double max: " + Double.MAX_VALUE);
+
+        System.out.println("\nFloat min: " + Float.MIN_VALUE);
+        System.out.println("Float max: " + Float.MAX_VALUE);
+
+        System.out.println("\nLong min: " + Long.MIN_VALUE);
+        System.out.println("Long max: " + Long.MAX_VALUE);
+
+        System.out.println("\nInteger min: " + Integer.MIN_VALUE);
+        System.out.println("Integer max: " + Integer.MAX_VALUE);
+
+        System.out.println("\nShort min: " + Short.MIN_VALUE);
+        System.out.println("Short max: " + Short.MAX_VALUE);
+
+        System.out.println("\nByte min: " + Byte.MIN_VALUE);
+        System.out.println("Byte max: " + Byte.MAX_VALUE);
+
+    }
+}
