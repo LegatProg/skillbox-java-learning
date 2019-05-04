8d5a800e93096fef049546bf400a617f68b8f1f6
LegatProg <yazza@mail.ru>
2019-05-04 16:17:56 +0500

    5_3 fixed

--- modul_5/5_3/EmailSet/src/Loader.java
+++ modul_5/5_3/EmailSet/src/Loader.java
@@ -1,3 +1,5 @@
+import javax.mail.internet.AddressException;
+import javax.mail.internet.InternetAddress;
 import java.util.Scanner;
 import java.util.TreeSet;
 
@@ -6,35 +8,40 @@ public class Loader {
     private static TreeSet<String> emails;
 
     public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
+
         String command;
         emails = new TreeSet<>();
-        do {
-            System.out.println("\nType command (to view available commands type \"HELP\"):");
-            command = scanner.nextLine().trim();
-            switch (command) {
-                case "EXIT": {
-                    break;
-                }
-                case "HELP": {
-                    printCommands();
-                    break;
-                }
-                case "LIST": {
-                    printList();
-                    break;
-                }
-                default: {
-                    if (command.startsWith("ADD")) {
-                        addEntity(command);
-                    } else {
-                        System.out.println("Wrong command, try again!");
+        boolean isExit = false;
+        try (Scanner scanner = new Scanner(System.in)) {
+            do {
+                System.out.println("\nType command (to view available commands type \"HELP\"):");
+                command = scanner.nextLine().trim();
+                switch (command) {
+                    case "EXIT": {
+                        isExit = true;
+                        break;
                     }
-                    break;
+                    case "HELP": {
+                        printCommands();
+                        break;
+                    }
+                    case "LIST": {
+                        printList();
+                        break;
+                    }
+                    default: {
+                        if (command.startsWith("ADD")) {
+                            addEntity(command);
+                        } else {
+                            System.out.println("Wrong command, try again!");
+                        }
+                        break;
+                    }
                 }
-            }
-        } while (!command.equals("EXIT"));
-        scanner.close();
+            } while (!isExit);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
     }
 
     private static void printList() {
@@ -43,18 +50,29 @@ public class Loader {
 
     private static void addEntity(String command) {
         String[] strings = command.split("\\s");
-        if (strings[1].matches("^\\w+@[a-zA-Z_]+\\.[a-zA-Z]{2,5}$")) {
-            emails.add(strings[1]);
-            System.out.println("Email added.");
+        if (strings.length == 2 && isValidEmailAddress(strings[1])) { //or strings.length > 1 && isValidEmailAddress(strings[1]) best ?
+            emails.add(strings[1]);                                   //but in this case we can write for ex. ADD yaaz@mail.com something else
+            System.out.println("Email added.");                       //and ADD the email anyway
         } else {
             System.out.println("Wrong email format!");
         }
     }
 
     private static void printCommands() {
-        System.out.println("Type \"ADD {email}\" to add email.");
+        System.out.println("Type \"ADD {email}\" to add the email.");
         System.out.println("Type \"LIST \" to print the list.");
         System.out.println("Type \"HELP\" to view available commands.");
         System.out.println("Type \"EXIT\" for exit.");
+    }
+
+    public static boolean isValidEmailAddress(String email) {
+        boolean result = true;
+        try {
+            InternetAddress emailAddr = new InternetAddress(email);
+            emailAddr.validate();
+        } catch (AddressException ex) {
+            result = false;
+        }
+        return result;
     }
 }
