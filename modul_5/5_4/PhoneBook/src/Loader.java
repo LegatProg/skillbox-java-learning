b6d06e825efa30905fe0c8c6237191fd9a1163c4
LegatProg <yazza@mail.ru>
2019-05-04 16:38:00 +0500

    5_4 fixed

--- modul_5/5_4/PhoneBook/src/Loader.java
+++ modul_5/5_4/PhoneBook/src/Loader.java
@@ -1,21 +1,22 @@
-import java.util.Map;
 import java.util.Scanner;
 import java.util.TreeMap;
 
 public class Loader {
 
-    private static String data;
+    private static String inputString;
+    private static TreeMap<String, String> phoneBook;
+    private static Scanner scanner;
 
     public static void main(String[] args) {
-        TreeMap<String, String> phoneBook = new TreeMap<>();
-        Scanner scanner = new Scanner(System.in);
+        phoneBook = new TreeMap<>();
+        scanner = new Scanner(System.in);
         do {
             System.out.println("Please, type the number or the name:");
-            data = scanner.nextLine().trim();
-            if (data.equals("EXIT")) {
+            inputString = scanner.nextLine().trim();
+            if (inputString.equals("EXIT")) {
                 break;
             }
-            if (data.equals("LIST")) {
+            if (inputString.equals("LIST")) {
                 if (!phoneBook.isEmpty()) {
                     phoneBook.forEach((key, value) -> System.out.println(key + " " + value));
                 } else {
@@ -23,39 +24,42 @@ public class Loader {
                 }
                 continue;
             }
-            if (data.matches("^\\W*\\d+.*")) {
-                findOrAddUser(phoneBook, scanner);
+            if (isPhoneNumber(inputString)) {
+                findOrAddUser();
             } else {
-                findOrAddNumber(phoneBook, scanner);
+                findOrAddNumber();
             }
-
-        } while (!data.equals("EXIT"));
+        } while (!inputString.equals("EXIT"));
         scanner.close();
     }
 
-    private static void findOrAddNumber(Map<String, String> phoneBook, Scanner scanner) {
-        if (phoneBook.containsKey(data)) {
-            System.out.println("User's number is: " + phoneBook.get(data));
+    private static boolean isPhoneNumber (String inputString){
+        return inputString.matches("^\\W*\\d+.*");
+    }
+
+    private static void findOrAddNumber() {
+        if (phoneBook.containsKey(inputString)) {
+            System.out.println("User's number is: " + phoneBook.get(inputString));
         } else {
             System.out.println("Number not found.\nPlease type the number:");
             String phone = scanner.nextLine().trim().replaceAll("\\D", "");
-            phoneBook.put(data, phone);
+            phoneBook.put(inputString, phone);
             System.out.println("The number was added.");
         }
     }
 
-    private static void findOrAddUser(Map<String, String> phoneBook, Scanner scanner) {
-        data = data.replaceAll("\\D", "");
-        if (phoneBook.containsValue(data)) {
+    private static void findOrAddUser() {
+        inputString = inputString.replaceAll("\\D", "");
+        if (phoneBook.containsValue(inputString)) {
             phoneBook.forEach((key, value) -> {
-                if (value.equals(data)) {
+                if (value.equals(inputString)) {
                     System.out.println("The user's name is: " + key);
                 }
             });
         } else {
             System.out.println("User not found.\nPlease type the user's name:");
             String user = scanner.nextLine().trim();
-            phoneBook.put(user, data);
+            phoneBook.put(user, inputString);
             System.out.println("The user was added.");
         }
     }
