46bab0d24434478ff6c6028b18c8980876b2e6e4
LegatProg <yazza@mail.ru>
2019-05-06 12:14:01 +0500

    6_2 fixed

--- modul_6/6_2/AbstractClientTest/src/bank/clients/IndEnterpreneur.java
+++ modul_6/6_2/AbstractClientTest/src/bank/clients/IndEnterpreneur.java
@@ -4,10 +4,11 @@ public class IndEnterpreneur extends Client {
 
     private final double BIG_COMMISSION = 0.01;
     private final double SMALL_COMMISSION = 0.005;
+    private final int CHECK_AMOUNT = 1000;
 
     @Override
     public void depositMoney(long amount) {
-        if (amount < 1000) {
+        if (amount < CHECK_AMOUNT) {
             account += amount - amount * BIG_COMMISSION;
         } else {
             account += amount - amount * SMALL_COMMISSION;
